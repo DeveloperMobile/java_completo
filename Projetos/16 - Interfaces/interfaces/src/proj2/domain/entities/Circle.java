1cae6c3fe45303eee5842507818f247408d925c6
DevJavaAnd <tiago.pereira.vieira@gmail.com>
2025-02-15 18:12:57 -0300

    Interfaces: proj2 - solucao com classe abstrata

---
+++ Projetos/16 - Interfaces/interfaces/src/proj2/domain/entities/Circle.java
@@ -0,0 +1,17 @@
+package proj2.domain.entities;
+
+import proj2.domain.entities.enums.Color;
+
+public class Circle extends Shape {
+    private Double radius;
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ Projetos/16 - Interfaces/interfaces/src/proj2/domain/entities/enums/Color.java
@@ -0,0 +1,6 @@
+package proj2.domain.entities.enums;
+
+public enum Color {
+    BLACK,
+    WHITE
+}
