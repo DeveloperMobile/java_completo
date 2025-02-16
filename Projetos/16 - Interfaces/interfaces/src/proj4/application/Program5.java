6446e597a4c00ee1a3bace45c0c57cfc17cb8e73
DevJavaAnd <tiago.pereira.vieira@gmail.com>
2025-02-16 12:29:53 -0300

    Interfaces: proj4 - Comparable

---
+++ Projetos/16 - Interfaces/interfaces/src/proj4/application/Program5.java
@@ -0,0 +1,33 @@
+package proj4.application;
+
+import proj4.entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Program5 {
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<>();
+        String path = "//home//tiago//in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String employeeCsv = br.readLine();
+            while (employeeCsv != null) {
+                String[] fields = employeeCsv.split(",");
+                list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+                employeeCsv = br.readLine();
+            }
+            Collections.sort(list);
+            for (Employee emp : list) {
+                System.out.println(emp.getName() + ", " + emp.getSalary());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
