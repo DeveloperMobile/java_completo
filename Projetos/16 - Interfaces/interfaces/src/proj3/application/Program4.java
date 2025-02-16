e09ba35a6ee5dbc2b0ba9f38b1c9b0b048e73026
DevJavaAnd <tiago.pereira.vieira@gmail.com>
2025-02-16 12:01:02 -0300

    Interfaces: proj3 - ComboDevice

--- Projetos/16 - Interfaces/interfaces/src/proj3/application/Program4.java
+++ Projetos/16 - Interfaces/interfaces/src/proj3/application/Program4.java
@@ -1,16 +1,26 @@
 package proj3.application;
 
-import proj3.devices.Printer;
-import proj3.devices.Scanner;
+import proj3.devices.ComboDevice;
+import proj3.devices.ConcretePrinter;
+import proj3.devices.ConcreteScanner;
 
 public class Program4 {
     public static void main(String[] args) {
-        Printer p = new Printer("1080");
+        ConcretePrinter p = new ConcretePrinter("1080");
         p.processDoc("My Letter");
         p.print("My Letter");
 
-        Scanner s = new Scanner("2003");
+        System.out.println();
+
+        ConcreteScanner s = new ConcreteScanner("2003");
         s.processDoc("My Email");
         System.out.println("Scan result: " + s.scan());
+
+        System.out.println();
+
+        ComboDevice c = new ComboDevice("2081");
+        c.processDoc("My dissertation");
+        c.print("My dissertation");
+        System.out.println("Scan rsult: " + c.scan());
     }
 }
