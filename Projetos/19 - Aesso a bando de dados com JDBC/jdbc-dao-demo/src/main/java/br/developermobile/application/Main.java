e092f58b8a17d70fac88b191629e5a30d3cb21ea
`Tiago <tiago.pereira.vieira@gmail.com>
2025-03-19 23:02:49 -0300

    Seller entity class

--- Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
+++ Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
@@ -1,10 +1,14 @@
 package br.developermobile.application;
 
 import br.developermobile.model.entities.Department;
+import br.developermobile.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
-        System.out.println(department);
+        Seller seller = new Seller(1,"Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
