4d355303c2f37cd4a236fffa605ce6dfb46e1f37
`Tiago <tiago.pereira.vieira@gmail.com>
2025-03-21 23:19:13 -0300

    findById implementation

--- Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
+++ Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
@@ -1,14 +1,13 @@
 package br.developermobile.application;
 
-import br.developermobile.model.entities.Department;
+import br.developermobile.model.dao.DaoFactory;
+import br.developermobile.model.dao.SellerDao;
 import br.developermobile.model.entities.Seller;
-
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        Seller seller = new Seller(1,"Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
--- Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/model/dao/impl/SellerDaoJDBC.java
+++ Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package br.developermobile.model.dao.impl;
 
+import br.developermobile.db.DB;
+import br.developermobile.db.DbException;
 import br.developermobile.model.dao.SellerDao;
+import br.developermobile.model.entities.Department;
 import br.developermobile.model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Seller seller) {
 
@@ -23,11 +36,48 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*, department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Department dep = instantiateDepartment(rs);
+                return instantiateSeller(rs, dep);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
     public List<Seller> findAll() {
         return List.of();
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        return new Seller(
+                rs.getInt("Id"),
+                rs.getString("Name"),
+                rs.getString("Email"),
+                rs.getDate("BirthDate"),
+                rs.getDouble("BaseSalary"),
+                dep);
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        return new Department(
+                rs.getInt("DepartmentId"),
+                rs.getString("DepName"));
     }
 }
