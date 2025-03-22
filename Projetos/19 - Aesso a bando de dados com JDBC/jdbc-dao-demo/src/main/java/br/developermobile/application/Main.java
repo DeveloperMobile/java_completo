d4acafb3b07f542aa45cfa6e1fd298f1a6de33a7
`Tiago <tiago.pereira.vieira@gmail.com>
2025-03-22 11:14:33 -0300

    update implementation

--- Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
+++ Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
@@ -25,9 +25,16 @@ public class Main {
         list = sellerDao.findAll();
         list.forEach(System.out::println);
 
-        System.out.println("\n=== TEST 3: seller insert ====");
+        System.out.println("\n=== TEST 4: seller insert ====");
         Seller newSeller = new Seller(null, "Gret", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        seller.setEmail("martha@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
