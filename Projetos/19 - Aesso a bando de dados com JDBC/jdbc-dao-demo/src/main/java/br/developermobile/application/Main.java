985287316a761071bc089b0f62f103c08bad7d6b
`Tiago <tiago.pereira.vieira@gmail.com>
2025-03-21 23:53:46 -0300

    findByDepartment implementation

--- Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
+++ Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/application/Main.java
@@ -2,12 +2,22 @@ package br.developermobile.application;
 
 import br.developermobile.model.dao.DaoFactory;
 import br.developermobile.model.dao.SellerDao;
+import br.developermobile.model.entities.Department;
 import br.developermobile.model.entities.Seller;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        list.forEach(System.out::println);
     }
 }
--- Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/model/dao/SellerDao.java
+++ Projetos/19 - Aesso a bando de dados com JDBC/jdbc-dao-demo/src/main/java/br/developermobile/model/dao/SellerDao.java
@@ -1,5 +1,6 @@
 package br.developermobile.model.dao;
 
+import br.developermobile.model.entities.Department;
 import br.developermobile.model.entities.Seller;
 
 import java.util.List;
@@ -10,4 +11,5 @@ public interface SellerDao {
     void delete(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
 }
