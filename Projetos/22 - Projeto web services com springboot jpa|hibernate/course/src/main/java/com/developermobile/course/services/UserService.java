1299fffa9cd5bcdccc0aca00787cfd4fff4cd05d
`Tiago <tiago.pereira.vieira@gmail.com>
2025-05-14 10:28:53 -0300

    Secao 22 - Exception handling - update

--- Projetos/22 - Projeto web services com springboot jpa|hibernate/course/src/main/java/com/developermobile/course/services/UserService.java
+++ Projetos/22 - Projeto web services com springboot jpa|hibernate/course/src/main/java/com/developermobile/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.developermobile.course.entities.User;
 import com.developermobile.course.repositories.UserRepository;
 import com.developermobile.course.services.exception.DatabaseException;
 import com.developermobile.course.services.exception.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -51,9 +52,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
