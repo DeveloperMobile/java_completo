d9846e025d3c5815d6e429393c9e9745b603676b
`Tiago <tiago.pereira.vieira@gmail.com>
2025-07-23 18:13:50 -0300

    Secao 24 - Primeiro commit - Projeto criado

---
+++ Projetos/24 - Projeto Spring Boot e banco MongoDB/workshopmongo/src/test/java/com/developermobile/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developermobile.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
