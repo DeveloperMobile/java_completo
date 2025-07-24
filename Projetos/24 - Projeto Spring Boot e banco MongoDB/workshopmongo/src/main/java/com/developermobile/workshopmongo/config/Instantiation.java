e46add0d802cd0a6745a286268b74cac6accdafc
`Tiago <tiago.pereira.vieira@gmail.com>
2025-07-24 17:07:29 -0300

    Secao 24 - Operação de instanciação da base de dados

---
+++ Projetos/24 - Projeto Spring Boot e banco MongoDB/workshopmongo/src/main/java/com/developermobile/workshopmongo/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.developermobile.workshopmongo.config;
+
+import com.developermobile.workshopmongo.UserRepository;
+import com.developermobile.workshopmongo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repository.deleteAll();
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
